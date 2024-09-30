c96ab6e76f79165934591ed385b0af0f8a8568a9
m0rfey <plaksinaleks2306@mail.ru>
2024-09-30 14:21:02 +0500

    # А как делить?

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divine(first, second):
+    if second == 0:
+        return 'Ошибка! Введите делитель не равный 0!'
+    else:
+        return first / second
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+# А как делить?
+from true_math import divine as tm
+from fake_math import divine as fm
+print (tm(10, 2))
+print (tm(10, 0))
+print (fm(10, 2))
+print (fm(10, 0))
